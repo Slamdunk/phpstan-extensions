6fc7cb96a1b40d17dd2521d8ea594b231d63ba85
Filippo Tessarotto <zoeslam@gmail.com>
2019-05-10 09:45:19 +0200

    Add AccessStaticPropertyWithinModelContextRule and create Yii config (#7)
    * Add AccessStaticPropertyWithinModelContextRule
    
    * Mirtes review
    
    * Minor improvements

---
+++ tests/AccessStaticPropertyWithinModelContextRuleTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SlamPhpStan\Tests;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use SlamPhpStan\AccessStaticPropertyWithinModelContextRule;
+
+/**
+ * @covers \SlamPhpStan\AccessStaticPropertyWithinModelContextRule
+ */
+final class AccessStaticPropertyWithinModelContextRuleTest extends RuleTestCase
+{
+    /**
+     * @var string
+     */
+    private $modelBaseClassOrInterface;
+
+    /**
+     * @var string
+     */
+    private $singletonAccessor;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        $this->modelBaseClassOrInterface = TestAsset\YiiAlikeActiveRecordInterface::class;
+        $this->singletonAccessor         = TestAsset\YiiAlikeBaseYii::class;
+
+        parent::__construct($name, $data, $dataName);
+    }
+
+    protected function getRule(): Rule
+    {
+        $broker = $this->createBroker();
+
+        return new AccessStaticPropertyWithinModelContextRule(
+            $broker,
+            $this->modelBaseClassOrInterface,
+            $this->singletonAccessor
+        );
+    }
+
+    public function testRule()
+    {
+        $this->analyse(
+            [
+                __DIR__ . '/TestAsset/AccessStaticPropertyWithinModelContextRule.php',
+            ],
+            [
+                [
+                    \sprintf('Class %s implements %s and uses %s::$app: in a model accessing to a singleton is considered an anti-pattern',
+                        TestAsset\ModelAccessingYiiAppSingletons::class,
+                        $this->modelBaseClassOrInterface,
+                        $this->singletonAccessor
+                    ),
+                    8,
+                ],
+                [
+                    \sprintf('Class %s implements %s and uses %s::$app: in a model accessing to a singleton is considered an anti-pattern',
+                        TestAsset\ModelAccessingYiiAppSingletons::class,
+                        $this->modelBaseClassOrInterface,
+                        $this->singletonAccessor
+                    ),
+                    10,
+                ],
+            ]
+        );
+    }
+}
